2ce7e81efe895cfdeff8dfbc2679a7fcd76f39dc
Brye Walker <bryewalks@gmail.com>
2026-01-24 19:25:54 -0600

    Add laptop and disko

--- flake.nix
+++ flake.nix
@@ -2,16 +2,26 @@
   description = "Brye's NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
+    disko,
     ...
   }:
 
@@ -24,20 +34,43 @@
         ./nixos/profiles/base.nix
         ./nixos/profiles/hyprland.nix
 
-	./nixos/hosts/${hostName}/hardware.nix
-	./nixos/hosts/${hostName}/default.nix
+	      ./nixos/hosts/${hostName}/hardware.nix
+	      ./nixos/hosts/${hostName}/default.nix
 
-	home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.users.brye = 
-	    import ./home/users/brye/default.nix;
-	}
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.brye = 
+            import ./home/users/brye/default.nix;
+        }
+      ];
+    };
+
+    mkHostWithDisko = hostName: nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        disko.nixosModules.disko
+        ./nixos/hosts/${hostName}/disko.nix
+
+        ./nixos/profiles/base.nix
+        ./nixos/profiles/hyprland.nix
+
+	      ./nixos/hosts/${hostName}/hardware.nix
+	      ./nixos/hosts/${hostName}/default.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.brye = 
+            import ./home/users/brye/default.nix;
+        }
       ];
     };
   in
   {
     nixosConfigurations.vm = mkHost "vm";
+    nixosConfigurations.laptop = mkHostWithDisko "laptop";
   };
 }
---
+++ nixos/hosts/laptop/disko.nix
@@ -0,0 +1,57 @@
+{ ... }:
+
+{
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+
+      device = "/dev/nvme0n1";
+
+      content = {
+        type = "gpt";
+        partitions = {
+          esp = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "cryptroot";
+              settings.allowDiscards = true;
+
+              content = {
+                type = "btrfs";
+                subvolumes = {
+                  "@" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "@persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
