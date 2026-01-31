919b8c91f61703b8ecc94270aeb54086aa771fdc
Brye Walker <bryewalks@gmail.com>
2026-01-31 17:51:18 -0600

    Add dracula vars

--- home/profiles/stylix/default.nix
+++ home/profiles/stylix/default.nix
@@ -1,6 +1,34 @@
 { config, pkgs, ... }:
 
 {
+  let
+    dracula = {
+      background = "#282a36";
+      lightBackground = "#343746";
+      lightForeGround = "#f1f2f8"
+      foreground = "#f8f8f2";
+      darkForeground = "#9ea8c7";
+      currentLine = "#6272a4"
+      selection = "#44475a";
+      red = "#ff5555";
+      orange = "#ffb86c";
+      green = "#50fa7b";
+      yellow = "#f1fa8c";
+      blue = "#bd93f9";
+      magenta = "#ff79c6";
+      cyan = "#8be9fd";
+      white = "#f8f8f2";
+      brightRed = "#ff6e6e";
+      brightOrange = "#ffd185"
+      brightGreen = "#69ff94";
+      brightYellow = "#ffffa5";
+      brightBlue = "#d6acff";
+      brightMagenta = "#ff92df";
+      brightCyan = "#a4ffff";
+      brightWhite = "#ffffff";
+    };
+  in 
+
   stylix = {
     enable = true;
     autoEnable = true;
@@ -16,22 +44,22 @@
 
     base16Scheme = {
       schema = "withHashTag";
-      base00 = "#282a36";
-      base01 = "#343746";
-      base02 = "#44475a";
-      base03 = "#6272a4";
-      base04 = "#9ea8c7";
-      base05 = "#f8f8f2";
-      base06 = "#f1f2f8";
-      base07 = "#ffffff";
-      base08 = "#ff5555";
-      base09 = "#ffb86c";
-      base0A = "#f1fa8c";
-      base0B = "#50fa7b";
-      base0C = "#8be9fd";
-      base0D = "#bd93f9";
-      base0E = "#ff79c6";
-      base0F = "#ff6e6e";
+      base00 = dracula.background;
+      base01 = dracula.lightBackground;
+      base02 = dracula.selection;
+      base03 = dracula.currentLine;
+      base04 = dracula.darkForeground;
+      base05 = dracula.foreground;
+      base06 = dracula.lightForeGround;
+      base07 = dracula.brightWhite;
+      base08 = dracula.red;
+      base09 = dracula.orange;
+      base0A = dracula.yellow;
+      base0B = dracula.green;
+      base0C = dracula.cyan;
+      base0D = dracula.blue;
+      base0E = dracula.magenta;
+      base0F = dracula.brightRed;
     };
 
     targets.kitty.enable = true;
