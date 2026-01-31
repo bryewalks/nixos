15ba6c1a7e300bc3408bf2abe3ca55231e7f9375
Brye Walker <bryewalks@gmail.com>
2026-01-30 20:02:19 -0600

    formatting

--- home/profiles/cli/default.nix
+++ home/profiles/cli/default.nix
@@ -9,9 +9,9 @@
     initContent = "source ~/.p10k.zsh";
     plugins = [
       {
-	name = "powerlevel10k";
-	src = pkgs.zsh-powerlevel10k;
-	file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
     ];
   };
