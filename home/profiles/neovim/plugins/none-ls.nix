4e7f159b6fd8ea97ea688a5514c1065f08c9e65c
bryewalks <bryewalks@gmail.com>
2026-02-01 23:40:56 -0600

    Fix again

--- home/profiles/neovim/plugins/none-ls.nix
+++ home/profiles/neovim/plugins/none-ls.nix
@@ -18,21 +18,19 @@
 
           diagnostics = {
             rubocop.enable = true;
-            # eslint_d comes from none-ls-extras
             eslint_d.enable = true;
           };
         };
       };
     };
 
-    keymaps = [{
-      mode = "n";
-      key = "<leader>cf";
-      action = "<Cmd>lua vim.lsp.buf.format()<CR>";
-      options.desc = "Code Format";
-    }];
-
-    # Dependencies
-    extraPlugins = with pkgs.nixvimPlugins; [ none-ls-extras-nvim ];
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>cf";
+        action = "<Cmd>lua vim.lsp.buf.format()<CR>";
+        options.desc = "Code Format";
+      }
+    ];
   };
 }
