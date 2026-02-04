7b4183b5747594a1e406b1b942ad738a110e8758
bryewalks <bryewalks@gmail.com>
2026-02-04 01:10:49 -0600

    Add import-tree module

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "Brye's NixOS";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
 
     disko = {
       url = "github:nix-community/disko";
@@ -24,6 +26,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    import-tree = {
+      url = "github:vic/import-tree";
+    };
+
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -35,69 +41,48 @@
     };
   };
 
-  outputs = { disko, home-manager, impermanence, nixpkgs, nixvim, self, sops-nix
-    , stylix, ... }:
+  outputs =
+    {
+      disko,
+      home-manager,
+      impermanence,
+      import-tree,
+      nixpkgs,
+      nixvim,
+      self,
+      sops-nix,
+      stylix,
+      ...
+    }:
 
     let
       system = "x86_64-linux";
 
-      mkHost = hostName:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            sops-nix.nixosModules.sops
-
-            ./nixos/hosts/${hostName}
-            ./nixos/hosts/${hostName}/hardware.nix
-
-            ./nixos/profiles
-            ./nixos/profiles/sops
-            ./nixos/profiles/sddm
-            ./nixos/profiles/hyprland
-            ./nixos/profiles/whitelist
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.sharedModules = [ nixvim.homeModules.nixvim ];
-              home-manager.users.brye = import ./home/users/brye;
-            }
-          ];
-        };
-
-      mkHostWithImpermanence = hostName:
+      mkHostWithImpermanence =
+        hostName:
         nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
             disko.nixosModules.disko
             impermanence.nixosModules.impermanence
             sops-nix.nixosModules.sops
-
-            ./nixos/hosts/${hostName}
-            ./nixos/hosts/${hostName}/hardware.nix
-            ./nixos/hosts/${hostName}/disko.nix
-
-            ./nixos/profiles
-            ./nixos/profiles/impermanence
-            ./nixos/profiles/sops
-            ./nixos/profiles/sddm
-            ./nixos/profiles/hyprland
-            ./nixos/profiles/whitelist
-
+            (import-tree ./nixos/hosts/${hostName})
+            (import-tree ./nixos/profiles)
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.sharedModules =
-                [ nixvim.homeModules.nixvim stylix.homeModules.stylix ];
+              home-manager.sharedModules = [
+                nixvim.homeModules.nixvim
+                stylix.homeModules.stylix
+              ];
               home-manager.users.brye = import ./home/users/brye;
             }
           ];
         };
-    in {
+    in
+    {
       diskoConfigurations.laptop = import ./nixos/hosts/laptop/disko.nix;
-      nixosConfigurations.vm = mkHost "vm";
       nixosConfigurations.laptop = mkHostWithImpermanence "laptop";
     };
 }
