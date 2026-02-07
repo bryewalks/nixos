c5158a11f8cbdff257ed38701d0ed00fe5cc7b3b
Brye Walker <bryewalks@gmail.com>
2026-02-07 01:42:14 -0600

    autostart fixes

--- home/modules/hyprland/autostart.nix
+++ home/modules/hyprland/autostart.nix
@@ -9,9 +9,9 @@
       "dropbox-cli start"
       "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
       "kbuildsycoca6"
-      "[workspace special:email silent] proton-mail"
-      "[workspace special:email silent] $webapp=\"https://gmail.com\""
-      "[workspace special:gpt silent] $webapp=\"https://chatgpt.com\""
+      "[workspace special:email silent] $browser --app=https://mail.proton.me"
+      "[workspace special:email silent] $browser --app=https://gmail.com"
+      "[workspace special:gpt silent] $browser --app=https://chatgpt.com"
       "[workspace special:terminal silent] $terminal tmux"
       "[workspace special:browser silent] firefox"
     ];
--- home/modules/hyprland/rules.nix
+++ home/modules/hyprland/rules.nix
@@ -4,8 +4,6 @@
     windowrulev2 = [
       "suppressevent maximize,class:.*"
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-      "workspace special:email silent,class:^(chrome-gmail.com__-Default)$"
-      "workspace special:gpt silent,class:^(chrome-chatgpt.com__-Default)$"
     ];
   };
 }
