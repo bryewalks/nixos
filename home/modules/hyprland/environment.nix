6a00f5fa71cfd0beff3bfdb8f6197013847d4665
bryewalks <bryewalks@gmail.com>
2026-03-04 21:05:02 -0600

    Electron wayland note

--- home/modules/hyprland/environment.nix
+++ home/modules/hyprland/environment.nix
@@ -6,6 +6,9 @@
       "QT_QPA_PLATFORM,wayland"
       "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
       "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+      # Force Electron apps to use wayland instead of xwayland.
+      # Currently push to talk is broken on wayland for discord.
+      # "NIXOS_OZONE_WL,1"
     ];
   };
 }
